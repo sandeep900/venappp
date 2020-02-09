acb37b405a6ef84f6095e6b0f2de855d7f384ccb
SANDEEP <SANDEEP@sandeep>
2020-02-09 16:20:08 +0530

    1st file change

---
+++ src/venappp/student.java
@@ -0,0 +1,15 @@
+package venappp;
+
+public class student {
+	
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
